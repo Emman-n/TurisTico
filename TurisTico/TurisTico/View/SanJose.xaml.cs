9e79db529355952c71091c75bac14619a18eefdc
Dcoream <direy23@gmail.com>
2022-04-04 12:27:23 -0600

    Prueba 10

---
+++ TurisTico/TurisTico/View/SanJose.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TurisTico.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TurisTico
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SanJose : ContentPage
+    {
+        public SanJose()
+        {
+            InitializeComponent();
+            BindingContext = new LugaresViewModel();
+        }
+    }
+}
