570cb33495897c2d1b844f53a627e26cdf286e04
Emman-n <enuneza937@gmail.com>
2022-04-04 11:38:07 -0600

    Correccion al test

--- TurisTico/TurisTico/App.xaml.cs
+++ TurisTico/TurisTico/App.xaml.cs
@@ -11,7 +11,7 @@ namespace TurisTico
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new MainPage());
+          ///  MainPage = new NavigationPage(new MainPage());
 
         }
 
