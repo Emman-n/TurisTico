e22f07c54c43f3d6aca7ef784c1388f6301bbc7a
Emman-n <enuneza937@gmail.com>
2022-04-04 11:35:21 -0600

    Categrias + lista de lugares + enviar a test

--- TurisTico/TurisTico/View/Categorias.xaml.cs
+++ TurisTico/TurisTico/View/Categorias.xaml.cs
@@ -4,24 +4,30 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
- 
+using TurisTico.ViewModels;
 using Xamarin.Forms;
 
 namespace TurisTico
 {
     public partial class MainPage : ContentPage
     {
+        
         public MainPage()
         {
             InitializeComponent();
-           
+            BindingContext = new LugaresViewModel();
 
         }
 
-        public double ColumnSpacing { get; set; }
+       
 
+         
+        private async void btnSanJose_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SanJose());
+            BindingContext = new LugaresViewModel();
 
-
+        }
 
     }
 }
