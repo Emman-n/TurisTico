6cb7ae5bf7b8d48f7d66a4cc9feea2cc34eb415d
Emman-n <enuneza937@gmail.com>
2022-04-04 22:30:25 -0600

    Ultimos cambios

--- TurisTico/TurisTico/ViewModels/LugaresViewModel.cs
+++ TurisTico/TurisTico/ViewModels/LugaresViewModel.cs
@@ -21,72 +21,71 @@ namespace TurisTico.ViewModels
 
                 new Product
                 {
-                    Nombre = "Nombre largo 1234568910",
-                    Lugar = "lugar",
-                    Image = "img1.jpg",
+                    Nombre = "Four Seasons Resort",
+                    Lugar = "Papagayo",
+                    Image = "lugar1.jpg",
 
-                    Precio = 111
-
-                },
-
-                new Product
-                {
-                    Nombre = "a",
-                    Lugar = "b",
-                    Image = "img1.jpg",
-
-                    Precio = 111
+                    Precio = 500
 
                 },
 
                 new Product
                 {
-                    Nombre = "a",
-                    Lugar = "b",
-                    Image = "img1.jpg",
+                    Nombre = "W Costa Rica",
+                    Lugar = "Reserva Conchal",
+                    Image = "lugar2.jpg",
 
-                    Precio = 111
-
-                },
-
-                new Product
-                {
-                    Nombre = "a",
-                    Lugar = "b",
-                    Image = "img1.jpg",
-
-                    Precio = 111
+                    Precio = 200
 
                 },
 
                 new Product
                 {
-                    Nombre = "a",
-                    Lugar = "b",
-                    Image = "img1.jpg",
+                    Nombre = "Altos de Eros ",
+                    Lugar = "Tamarindo",
+                    Image = "lugar3.jpg",
 
-                    Precio = 111
+                    Precio = 250
+
+                },
+
+                new Product
+                {
+                    Nombre = "Dreams Las Mareas",
+                    Lugar = "Playa El Jobo",
+                   Image = "lugar4.jpg",
+
+                    Precio = 234
+                },
+
+                new Product
+                {
+                    Nombre = "Fenix Hotel",
+                    Lugar = "Playa Samara",
+                    Image = "lugar5.jpg",
+
+                    Precio = 77
 
                 },
 
                new Product
                 {
-                    Nombre = "a",
-                    Lugar = "b",
-                    Image = "montanas.jpg",
+                    Nombre = "Villa Buena Onda",
+                    Lugar = "Playas del Coco",
+                    Image = "lugar6.jpg",
 
-                    Precio = 111
+                    Precio = 80
 
                 },
 
 
                new Product
                 {
-                    Nombre = "a",
-                    Lugar = "b",
-                    Image = "montanas.jpg",
+                    Nombre = "Margaritaville ",
+                    Lugar = "Playa Flamingo",
+                    Image = "lugar7.jpg",
 
-                    Precio = 111
+                    Precio = 120
 
                 },
 
@@ -94,6 +93,8 @@ namespace TurisTico.ViewModels
 
 
             };
+
+
         }
 
     }
